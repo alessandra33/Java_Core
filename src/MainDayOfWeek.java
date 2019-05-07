1a7484a02b1e425b8e389e3a96f54bbe534b935f
alessandra33 <aleksandraevgenevna33@gmail.com>
2019-05-07 23:38:07 +0300

    Lesson 2

---
+++ src/MainDayOfWeek.java
@@ -0,0 +1,13 @@
+public class MainDayOfWeek {
+    public static int getWorkingHours(DayOfWeek dayOfWeek) {
+        int result = 0;
+        for(int i = dayOfWeek.ordinal(); i < dayOfWeek.values().length; i++ )
+            result += dayOfWeek.values()[i].getHour();
+        return result;
+    }
+
+
+    public static void main(final String[] args) {
+        System.out.println(getWorkingHours(DayOfWeek.TUESDAY));
+    }
+}
